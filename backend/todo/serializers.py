ae46a4637aab902d04e8e7048756735637f69a85
Jenny Shi <jshi@zenput.com>
2022-06-24 18:25:32 -0400

    Set up Django backend and API

---
+++ backend/todo/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    # the thing you'll actually have access to
+    class Meta:
+        model = Todo
+        fields = ("id", "title", "description", "completed")
